a0fcff851e7bc83765da7e422e5960e5a7549543
Khachatur86 <35083057+Khachatur86@users.noreply.github.com>
2018-02-22 09:50:31 +0300

    Update lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -1,43 +1,64 @@
 class Animals():
     """Common class of animals"""
 
-    def __init__(self, name: object, legs: object, food: object) -> object:
-        self.name = ""
-        self.legs = 4
-        self.food = ""
+    def __init__(self, name, legs, food)
+        self.name = name
+        self.legs = legs
+        self.food = food
 
     def sound(self, voice):
-        self.voice = ""
+        self.voice = voice
         print(self.voice)
+        
+    def display(self, food):
+        self.food = food
+        print("I am eating a " , food) # Я бы хотел, чтобы не указывая в скобках переменную food оно работало правильно: оно бы брало food из __init__, возможно ли такое?
+    
 
 
-""""Class of birds"""
+"""Class of birds"""
 
 
 class Birds(Animals):
-    food = "corn"
+   def height_of_flight (self, height):
+    self.height = height
+    print(self.height)
 
 
-""""Class of Artodactils"""
+"""Class of Artodactils"""
 
 
 class Artiodactyls(Animals):
-    food = "grass"
+    def distance_of_running (self, distance):
+        self.distance = distance
 
-
-duck = Birds()
-duck.sound("Kryakrya")
-duck.__init__("Donald",4, "mais")
-hen = Birds()
-hen.__init__("Petya", 2, "wheat")
-hen.sound("Kukareku")
-goose = Birds()
-goose.sound("Gagaga")
-cow = Artiodactyls()
+        
+cow = Artiodactyls("Buurenka", 4, "grass")
 cow.sound("Muuu")
-goat = Artiodactyls()
+cow.display()
+
+goat = Artiodactyls("Marta", 4, "grass")
 goat.sound("beeeee")
-sheep = Artiodactyls()
+goat.display()
+
+sheep = Artiodactyls("Shon", 4, "grass")
 sheep.sound("beee")
-pig = Artiodactyls()
+sheep.display()
+
+pig = Artiodactyls("Funtic", 4, "acorn")
 pig.sound("khrukhru")
+pig.display()
+          
+
+duck = Birds("Donald", 2, "worm")
+duck.sound("Kryakrya")
+duck.display() 
+
+hen = Birds("Petya", 2, "bread crumbs")
+hen.sound("Kukareku")
+dusk.display()
+
+goose = Birds("Seriy", 2, "bread crumbs")
+goose.sound("Gagaga")
+goose.display()
+
