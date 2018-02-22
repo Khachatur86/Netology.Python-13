548e03e922496de25b9dfa58b88819b6ac097fdf
Khachatur86 <35083057+Khachatur86@users.noreply.github.com>
2018-02-22 10:55:57 +0300

    Corrections
    Some correction after compilation.

--- lesson7.py
+++ lesson7.py
@@ -1,18 +1,17 @@
 class Animals():
     """Common class of animals"""
 
-    def __init__(self, name, legs, food)
+    def __init__(self, name, legs, food):
         self.name = name
         self.legs = legs
         self.food = food
 
     def sound(self, voice):
         self.voice = voice
-        print(self.voice)
+        print("I cry",self.voice,".")
         
-    def display(self, food):
-        self.food = food
-        print("I am eating a " , food) # Я бы хотел, чтобы не указывая в скобках переменную food оно работало правильно: оно бы брало food из __init__, возможно ли такое?
+    def display(self):
+        print("I am eating a " , self.food,".") # Разобрался :) Я бы хотел, чтобы не указывая в скобках переменную food оно работало правильно: оно бы брало food из __init__, возможно ли такое?
     
 
 
@@ -22,7 +21,7 @@ class Animals():
 class Birds(Animals):
    def height_of_flight (self, height):
     self.height = height
-    print(self.height)
+    print("I can fly an",self.height, "metrs above the Earth.")
 
 
 """Class of Artodactils"""
@@ -31,35 +30,43 @@ class Birds(Animals):
 class Artiodactyls(Animals):
     def distance_of_running (self, distance):
         self.distance = distance
+        print("I can run a:", self.distance, "miles.")
 
         
+
+duck = Birds("Donald", 2, "worm")
+duck.sound("kryakrya")
+duck.display() 
+duck.height_of_flight(1700)
+
+hen = Birds("Petya", 2, "bread crumbs")
+hen.sound("kukareku")
+hen.display()
+hen.height_of_flight(10)
+
+goose = Birds("Seriy", 2, "bread crumbs")
+goose.sound("gagaga")
+goose.display()
+goose.height_of_flight(15)
+
 cow = Artiodactyls("Buurenka", 4, "grass")
-cow.sound("Muuu")
+cow.sound("muuu")
 cow.display()
+cow.distance_of_running(10)
 
 goat = Artiodactyls("Marta", 4, "grass")
 goat.sound("beeeee")
 goat.display()
+goat.distance_of_running(17)
 
 sheep = Artiodactyls("Shon", 4, "grass")
-sheep.sound("beee")
+sheep.sound("beebee")
 sheep.display()
+sheep.distance_of_running(9)
 
 pig = Artiodactyls("Funtic", 4, "acorn")
 pig.sound("khrukhru")
 pig.display()
-          
-
-duck = Birds("Donald", 2, "worm")
-duck.sound("Kryakrya")
-duck.display() 
-
-hen = Birds("Petya", 2, "bread crumbs")
-hen.sound("Kukareku")
-dusk.display()
-
-goose = Birds("Seriy", 2, "bread crumbs")
-goose.sound("Gagaga")
-goose.display()
+pig.distance_of_running(30)
 
 
